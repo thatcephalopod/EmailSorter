030eddaf78fe6012c4f01b21b80f939464459ad8
thatcephalopod <thatcephalopod.sam@gmail.com>
2016-12-14 20:02:54 -0500

    Added Classes with their respective methods

---
+++ EmailSorter/src/emailsorter/Email.java
@@ -0,0 +1,19 @@
+
+package emailsorter;
+
+/**
+ *
+ * @author Sam
+ */
+
+import java.util.ArrayList;
+
+public class Email {
+    
+    public String sender;
+    public String recipient;
+    public String subject;
+    public ArrayList<String> body = new ArrayList<String>();
+    public int value;
+    
+}
---
+++ EmailSorter/src/emailsorter/EmailHolder.java
@@ -0,0 +1,18 @@
+
+package emailsorter;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sam
+ */
+public class EmailHolder {
+    
+    public ArrayList<Email> emails;
+    
+    public void sort() {
+        
+    }
+    
+}
